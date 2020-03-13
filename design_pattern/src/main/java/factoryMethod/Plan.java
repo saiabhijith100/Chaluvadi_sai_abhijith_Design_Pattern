b55a3ad79c0c4eb7b6cb58c84c7f3be4aea920bf
sai nani <saiabhijith100@gmail.com>
2020-03-13 19:42:28 +0530

    First Commit

---
+++ design_pattern/src/main/java/factoryMethod/Plan.java
@@ -0,0 +1,10 @@
+package factoryMethod;
+
+abstract class Plan {
+    protected double rate;  
+    abstract void getRate();  
+
+    public double calculateBill(double Sqrfoot){  
+         return(Sqrfoot*rate);  
+     }  
+}
